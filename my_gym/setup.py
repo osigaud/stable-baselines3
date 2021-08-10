0127575a22c1cf46e7f700bacc00f7d417fb8fb0
Antonin Raffin <antonin.raffin@ensta.org>
2021-08-10 10:12:41 +0200

    Fix package name

--- my_gym/setup.py
+++ my_gym/setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='continuous_cartpole',
+setup(name="my_gym",
+      packages=[package for package in find_packages() if package.startswith("my_gym")],
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 )
