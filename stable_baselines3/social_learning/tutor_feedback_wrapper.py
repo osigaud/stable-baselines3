5af9b5f5b015e6c08f89ab9f207d85437ccc79f7
osigaud <Olivier.Sigaud@upmc.fr>
2021-11-03 08:37:57 +0100

    added comments

--- stable_baselines3/social_learning/tutor_feedback_wrapper.py
+++ stable_baselines3/social_learning/tutor_feedback_wrapper.py
@@ -1,6 +1,6 @@
 import gym
 
-# This is a custom wrapper for playing with reward shaping in the MountainCarContinuous environment
+# This is the reward shaping function that you may want to tune better
 def tutor_reward_signal(obs, reward):
     if 0 < obs[0] < 0.5:
         return 10
@@ -11,6 +11,7 @@ def tutor_reward_signal(obs, reward):
 class TutorFeedbackWrapper(gym.Wrapper):
     """
     :param env: (gym.Env) Gym environment that will be wrapped
+    # This is a custom wrapper for playing with reward shaping in the MountainCarContinuous environment
     """
 
     def __init__(self, env):
